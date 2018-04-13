2ed3a3ebdc7eb9e5a781b6412b4a61a81f7067de
suesanz <yadavsourav24071998@gmail.com>
2018-04-14 01:07:34 +0530

    Up
    Signed-off-by: suesanz <yadavsourav24071998@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const path=require('path')
 const app=express()
+const SERVER_PORT=process.env.PORT || 3333
 app.use('/public', express.static(path.join(__dirname,'public')))
 app.get('/',(req,res)=>res.send("Hello"))
-app.listen(2323)
+app.listen(SERVER_PORT)
